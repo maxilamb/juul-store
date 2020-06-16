2818e72cb4e3f227ef27552b2665b3dbb5ec7183
Maxim Lambov <maxilamb@yandex.com>
2020-06-17 00:16:16 +0300

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:prettier/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    'quotes': ['error', 'single', 'avoid-escape'],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'linebreak-style': 'off',
+    'arrow-parens': 'off',
+    'object-curly-newline': 'off',
+    'no-mixed-operators': 'off',
+    'arrow-body-style': 'off',
+    'function-paren-newline': 'off',
+    'no-plusplus': 'off',
+    'space-before-function-paren': 0,
+    'max-len': ['error', 100, 2, { ignoreUrls: true, }],
+    'no-console': 'error',
+    'no-alert': 'error',
+    'no-param-reassign': 'off',
+    'react/require-default-props': 'off',
+    'react/forbid-prop-types': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
+    'prefer-destructuring': 'off',
+    'react/no-find-dom-node': 'off',
+    'react/no-did-mount-set-state': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+
+    "jsx-a11y/anchor-is-valid": ["error", { "components": ["Link"], "specialLink": ["to"] }],
+    "jsx-a11y/label-has-for": [2, {
+      "required": {
+        "every": ["id"]
+      }
+    }],
+    'prettier/prettier': ['error'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
---
+++ README.md
@@ -0,0 +1,12 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Sample landing for sale juul
+
+contains pages:
+
+1. main page
+1. contact
+1. about
+1. faq
+1. order page
+1. payment success and error
