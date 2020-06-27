69c152028d3c6a1b9ba011404c4e3b03b0299f5b
Maxim Lambov <maxilamb@yandex.com>
2020-06-27 03:12:25 +0300

    added device section

--- src/components/common/article/index.js
+++ src/components/common/article/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style.sass';
 
-function Article({ header, text, muted = false}) {
+function Article({ header, text, muted = false, center }) {
+  const hClassName = center ? 'text-center' : '';
   const pClassName = muted ? 'text-muted' : '';
   return (
     <div className='article'>
-      <h3>{header}</h3>
+      <h3 className={hClassName}>{header}</h3>
       <p className={pClassName}>{text}</p>
     </div>
   );
 }
 
 Article.propTypes = {
-  header: PropTypes.string,
-  text: PropTypes.string,
-  muted: PropTypes.bool
+  header: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  center: PropTypes.bool,
+  muted: PropTypes.bool,
 };
 
 export default Article;
