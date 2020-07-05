015cc138f052ef11c7b23c5aa98f0e4c141463c9
Maxim Lambov <maxilamb@yandex.com>
2020-07-05 14:46:15 +0300

    added footer

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { LogoFooter } from '../logo';
+import './style.sass';
+
+function Footer() {
+  return (
+    <footer>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12 col-md-3 d-flex align-items-center justify-content-center'>
+            <Link to='/terms'>Пользовательское соглашение</Link>
+          </div>
+          <div className='col-12 col-md-6 d-flex justify-content-center align-items-center'>
+            <div>
+              <LogoFooter />
+              <small className='text-muted'>Все права защищены ©</small>
+            </div>
+          </div>
+          <div className='col-12 col-md-3'>
+            <ul className='list-group list-group-horizontal'>
+              <li className='list-group-item'>
+                <a href='https://www.instagram.com/wylsashop_'>
+                  <i title='Мы в инстаграм' className='fab fa-instagram' />
+                </a>
+              </li>
+              <li className='list-group-item'>
+                <a href='https://www.t.me/wylsashop_bot'>
+                  <i title='Напишите нам в telegram' className='fab fa-telegram-plane' />
+                </a>
+              </li>
+              <li className='list-group-item'>
+                <a href='/'>
+                  <i title='Напишите нам в viber' className='fab fa-viber' />
+                </a>
+              </li>
+              <li className='list-group-item'>
+                <a href='/'>
+                  <i title='Напишите нам в whatsapp' className='fab fa-whatsapp' />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
