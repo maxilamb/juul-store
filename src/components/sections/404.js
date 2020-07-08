11df2bd198e624d9f5fca08a626f740cdd56d6b5
Maxim Lambov <maxilamb@yandex.com>
2020-07-09 01:20:04 +0300

    added section page not found

---
+++ src/components/sections/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <section className='min-vh-100 d-flex justify-content-center align-items-center'>
+      <h1>Page Not Found</h1>
+    </section>
+  );
+}
+
+export default NotFound;
