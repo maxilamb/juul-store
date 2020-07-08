8be000fffc2e62cbccc8f4f5eabbf13271278db7
Maxim Lambov <maxilamb@yandex.com>
2020-07-09 01:24:51 +0300

    remove serviceworker

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'animate.css/animate.min.css';
 import Root from './root';
 import './style.sass';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
-
-serviceWorker.unregister();
