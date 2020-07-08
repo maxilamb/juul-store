f0ed5527620604065557b0882eca8998d4ba7e5e
Maxim Lambov <maxilamb@yandex.com>
2020-07-09 00:39:27 +0300

    added store

---
+++ src/store.js
@@ -0,0 +1,18 @@
+const store = {
+  price: 1999,
+  oldPrice: 2395,
+  formStage: 1,
+  amount: 1,
+  selectedColor: 'black',
+  fullname: '',
+  street: '',
+  house: '',
+  region: '',
+  city: '',
+  postalCode: '',
+  phone: '',
+  email: '',
+  country: 'Россия',
+};
+
+export default store;
