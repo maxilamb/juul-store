b45ddb07f24afa6e6e62d154dd82f7ad4b0455f2
Maxim Lambov <maxilamb@yandex.com>
2020-07-09 01:22:47 +0300

    initialized the input app point

---
+++ src/root.js
@@ -0,0 +1,40 @@
+import React, { Suspense, lazy, useReducer } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Context from './context';
+import Store from './store';
+import FormReducer from './reducers/form';
+import Header from './components/common/header';
+import Footer from './components/common/footer';
+import { LoaderFullScreen } from './components/common/stub/loader';
+import NotFound from './components/sections/404';
+
+const IndexPage = lazy(() => import('./components/routes/index-page'));
+const TermsAndConditionsPage = lazy(() => import('./components/routes/terms/terms-and-conditions'));
+const TermsOfUse = lazy(() => import('./components/routes/terms/terms-of-use'));
+
+function Root() {
+  const [store, dispatch] = useReducer(FormReducer, Store);
+  return (
+    <Suspense fallback={<LoaderFullScreen />}>
+      <Context.Provider
+        value={{
+          store,
+          dispatch,
+        }}
+      >
+        <Router>
+          <Header />
+          <Switch>
+            <Route path='/' component={IndexPage} exact />
+            <Route path='/termsAndConditions' component={TermsAndConditionsPage} />
+            <Route path='/terms' component={TermsOfUse} />
+            <Route path='*' component={NotFound} />
+          </Switch>
+          <Footer />
+        </Router>
+      </Context.Provider>
+    </Suspense>
+  );
+}
+
+export default Root;
