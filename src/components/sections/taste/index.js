31e64e8495aa26dca2ff5ed5dba94f17962285c9
Maxim Lambov <maxilamb@yandex.com>
2020-07-12 14:19:30 +0300

    wrapped animate element

--- src/components/sections/taste/index.js
+++ src/components/sections/taste/index.js
@@ -23,39 +23,39 @@ export default function Taste() {
     return () => Event.removeEventListener('scroll');
   });
   return (
-    <Element
-      component='section'
-      className={cx('px-lg-5 text-center animate__animated', {
-        animate__bounceInUp: animate,
-        animate__bounceOutDown: !animate,
-      })}
-      id='tastes'
-    >
-      <SectionHeader header='Только вкус ничего больше' text='Открывайте для себя вкусы JUUL' />
-      <div className='taste-list'>
-        <div className='taste-item'>
-          <img src={classicTaste} alt='' />
-          <span>Классический</span>
-        </div>
-        <div className='taste-item'>
-          <img src={mangoTaste} alt='' />
-          <span>Манго</span>
-        </div>
-        <div className='taste-item'>
-          <img src={mintTaste} alt='' />
-          <span>Мята</span>
-        </div>
-        <div className='taste-item'>
-          <img src={vanillaTaste} alt='' />
-          <span>Ваниль</span>
-        </div>
-        <div className='taste-item'>
-          <img src={virdgTaste} alt='' />
-          <span>Вирджиния</span>
-        </div>
-        <div className='taste-item'>
-          <img src={fruitsMixTaste} alt='' />
-          <span>Фруктовый микс</span>
+    <Element component='section' id='tastes' className='px-lg-5 text-center'>
+      <div
+        className={cx('px-lg-5 text-center animate__animated', {
+          animate__bounceInUp: animate,
+          animate__bounceOutDown: !animate,
+        })}
+      >
+        <SectionHeader header='Только вкус ничего больше' text='Открывайте для себя вкусы JUUL' />
+        <div className='taste-list'>
+          <div className='taste-item'>
+            <img src={classicTaste} alt='' />
+            <span>Классический</span>
+          </div>
+          <div className='taste-item'>
+            <img src={mangoTaste} alt='' />
+            <span>Манго</span>
+          </div>
+          <div className='taste-item'>
+            <img src={mintTaste} alt='' />
+            <span>Мята</span>
+          </div>
+          <div className='taste-item'>
+            <img src={vanillaTaste} alt='' />
+            <span>Ваниль</span>
+          </div>
+          <div className='taste-item'>
+            <img src={virdgTaste} alt='' />
+            <span>Вирджиния</span>
+          </div>
+          <div className='taste-item'>
+            <img src={fruitsMixTaste} alt='' />
+            <span>Фруктовый микс</span>
+          </div>
         </div>
       </div>
     </Element>
