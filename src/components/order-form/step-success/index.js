c32f46567e02fcefe0bb71d632250a044f87cd79
Maxim Lambov <maxilamb@yandex.com>
2020-07-12 22:20:26 +0300

    added step success

---
+++ src/components/order-form/step-success/index.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import Context from '../../../context';
+import ImgSticker from './sticker.webp';
+
+function StepSuccess() {
+  const { dispatch } = useContext(Context);
+  return (
+    <div className='step-three'>
+      <form
+        onSubmit={() => {
+          dispatch({
+            type: 'CHANGE_FORM_STAGE',
+            payload: 1,
+          });
+        }}
+      >
+        <h3>
+          Спасибо что заказали JUUL, мы скоро свяжемся с вами
+          <span role='img' aria-label='heart'>
+            💗
+          </span>
+        </h3>
+        <img className='w-100' src={ImgSticker} alt='sticker' />
+        <div className='form-group'>
+          <button type='submit' className='btn btn-outline-success btn-block rounded-pill btn-lg'>
+            Заказать ещё
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default StepSuccess;
