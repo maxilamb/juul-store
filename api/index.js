36a068376886f88fe2c4f9fd6c5f1ea0b1277261
maxilamb <maxilamb@yandex.com>
2020-07-15 07:20:23 +0300

    added an entry point to the app

--- api/index.js
+++ api/index.js
@@ -1,5 +1,30 @@
 const express = require('express');
+const helmet = require('helmet');
+const path = require('path');
+const morgan = require('morgan');
+
+const { PORT, NODE_ENV } = process.env;
 
 const app = express();
 
-app.listen(3000);
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (NODE_ENV === 'development') {
+  app.use(morgan('combined'));
+}
+
+require('./routes/feedback')(app);
+require('./routes/order')(app);
+require('./routes/review')(app);
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../', 'build', 'index.html'));
+});
+
+app.use((req, res) => res.sendStatus(404));
+
+const port = PORT || 5000;
+
+app.listen(port);
