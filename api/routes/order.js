ecfe61a11aa3d8d3b71887a97b76d2d7c1f88fdd
maxilamb <maxilamb@yandex.com>
2020-07-16 23:32:40 +0300

    added trim and escape

--- api/routes/order.js
+++ api/routes/order.js
@@ -5,23 +5,98 @@ module.exports = (app) => {
   app.post(
     '/api/v1/order',
     [
-      check('paymentMethod').not().isEmpty().withMessage('Field paymentMethod is empty'),
-      check('amount').isNumeric().withMessage('Field amount is empty'),
-      check('selectedColor').not().isEmpty().withMessage('Field selectedColor is empty'),
-      check('fullname').not().isEmpty().withMessage('Field fullname is empty'),
-      check('street').not().isEmpty().withMessage('Field street is empty'),
-      check('house').not().isEmpty().withMessage('Field house is empty'),
-      check('region').not().isEmpty().withMessage('Field region is empty'),
-      check('city').not().isEmpty().withMessage('Field city is empty'),
-      check('postalCode').not().isEmpty().withMessage('Field postalCode is empty'),
-      check('phone').not().isEmpty().withMessage('Field phone is empty'),
-      check('country').not().isEmpty().withMessage('Field country is empty'),
-      check('email')
+      check('paymentMethod', 'Обязательное поле')
+        .isString()
+        .withMessage('Поле paymentMethod должно быть string')
         .not()
         .isEmpty()
-        .withMessage('Field email is empty')
+        .withMessage('Поле paymentMethod не должно быть пустым')
+        .trim()
+        .escape(),
+      check('amount', 'Обязательное поле')
+        .isNumeric()
+        .withMessage('Поле amount должно быть числом')
+        .trim()
+        .escape(),
+      check('selectedColor', 'Обязательное поле')
+        .isString()
+        .withMessage('Поле selectedColor должно быть string')
+        .not()
+        .isEmpty()
+        .withMessage('Поле selectedColor не должно быть пустым')
+        .trim()
+        .escape(),
+      check('fullname', 'Обязательное поле')
+        .isString()
+        .withMessage('Поле fullname должно быть string')
+        .not()
+        .isEmpty()
+        .withMessage('Поле fullname не должно быть пустым')
+        .trim()
+        .escape(),
+      check('street', 'Обязательное поле')
+        .isString()
+        .withMessage('Поле street должно быть string')
+        .not()
+        .isEmpty()
+        .withMessage('Поле street не должно быть пустым')
+        .trim()
+        .escape(),
+      check('house', 'Обязательное поле')
+        .isString()
+        .withMessage('Поле house должно быть string')
+        .not()
+        .isEmpty()
+        .withMessage('Поле house не должно быть пустым')
+        .trim()
+        .escape(),
+      check('region', 'Обязательное поле')
+        .isString()
+        .withMessage('Поле region должно быть string')
+        .not()
+        .isEmpty()
+        .withMessage('Поле region не должно быть пустым')
+        .trim()
+        .escape(),
+      check('city', 'Обязательное поле')
+        .isString()
+        .withMessage('Поле city должно быть string')
+        .not()
+        .isEmpty()
+        .withMessage('Поле city не должно быть пустым')
+        .trim()
+        .escape(),
+      check('postalCode', 'Обязательное поле')
+        .isString()
+        .withMessage('Поле postalCode должно быть string')
+        .not()
+        .isEmpty()
+        .withMessage('Поле postalCode не должно быть пустым')
+        .trim()
+        .escape(),
+      check('phone', 'Обязательное поле')
+        .isString()
+        .withMessage('Поле phone должно быть string')
+        .not()
+        .isEmpty()
+        .withMessage('Поле phone не должно быть пустым')
+        .trim()
+        .escape(),
+      check('country', 'Обязательное поле')
+        .isString()
+        .withMessage('Поле country должно быть string')
+        .not()
+        .isEmpty()
+        .withMessage('Поле country не должно быть пустым')
+        .trim()
+        .escape(),
+      check('email', 'Обязательное поле')
+        .isString()
+        .withMessage('Поле email должно быть string')
         .isEmail()
-        .withMessage('Field email is not email'),
+        .withMessage('Поле email должно быть email')
+        .trim()
+        .escape(),
     ],
     post,
   );
