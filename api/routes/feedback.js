696fe7e5df7d55423b1c6aee8983a5717372cf5e
maxilamb <maxilamb@yandex.com>
2020-07-16 23:21:37 +0300

    added trim and escape

--- api/routes/feedback.js
+++ api/routes/feedback.js
@@ -8,21 +8,29 @@ module.exports = (app) => {
     [
       check('isContactForm', 'Обязательное поле')
         .isBoolean()
-        .withMessage('Поле isContactForm должно boolean'),
+        .withMessage('Поле isContactForm должно boolean')
+        .trim()
+        .escape(),
       check('fullname', 'Обязательное поле')
         .isString()
         .withMessage('Поле fullname должно быть string')
         .isLength({ min: 4 })
-        .withMessage('Поле fullname должно быть длинее 4 символов'),
+        .withMessage('Поле fullname должно быть длинее 4 символов')
+        .trim()
+        .escape(),
       check('track-code', 'Обязательное поле')
         .isString()
-        .withMessage('Поле track-code должно быть string'),
+        .withMessage('Поле track-code должно быть string')
+        .trim()
+        .escape(),
       check('email').isString().isEmail(),
       check('message', 'Обязательное поле')
         .isString()
         .withMessage('Поле message должно быть string')
         .isLength({ min: 10 })
-        .withMessage('Поле message должно быть длинее 10 символов'),
+        .withMessage('Поле message должно быть длинее 10 символов')
+        .trim()
+        .escape(),
     ],
     post,
   );
