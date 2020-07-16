9f5261cf7645eb0ec7af026f6a6934b1626156c3
maxilamb <maxilamb@yandex.com>
2020-07-16 23:11:26 +0300

    added route for feedback method http post with validation data

---
+++ api/routes/feedback.js
@@ -0,0 +1,29 @@
+const { check } = require('express-validator');
+
+const post = require('../models/feedback/post');
+
+module.exports = (app) => {
+  app.put(
+    '/api/v1/feedpack',
+    [
+      check('isContactForm', 'Обязательное поле')
+        .isBoolean()
+        .withMessage('Поле isContactForm должно boolean'),
+      check('fullname', 'Обязательное поле')
+        .isString()
+        .withMessage('Поле fullname должно быть string')
+        .isLength({ min: 4 })
+        .withMessage('Поле fullname должно быть длинее 4 символов'),
+      check('track-code', 'Обязательное поле')
+        .isString()
+        .withMessage('Поле track-code должно быть string'),
+      check('email').isString().isEmail(),
+      check('message', 'Обязательное поле')
+        .isString()
+        .withMessage('Поле message должно быть string')
+        .isLength({ min: 10 })
+        .withMessage('Поле message должно быть длинее 10 символов'),
+    ],
+    post,
+  );
+};
