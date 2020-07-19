83efb76affe094ae0de5eb2e18228792f0bdc729
maxilamb <maxilamb@yandex.com>
2020-07-19 19:38:54 +0300

    added model for http put method

--- api/routes/review.js
+++ api/routes/review.js
@@ -1,5 +1,21 @@
+const { check } = require('express-validator');
 const get = require('../models/review/get');
+const put = require('../models/review/put');
 
 module.exports = (app) => {
   app.get('/api/v1/review', get);
+
+  app.put(
+    '/api/v1/review',
+    [
+      check('id', 'Обязательное поле').isMongoId().withMessage('Не правильный id'),
+      check('vote', 'Обязательное поле')
+        .isString()
+        .withMessage('Поле vote должно быть строкой')
+        .not()
+        .isEmpty()
+        .withMessage('Поле vote не должно быть пустым'),
+    ],
+    put,
+  );
 };
