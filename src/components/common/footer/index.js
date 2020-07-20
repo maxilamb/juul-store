547f96ab6573ba084a18cb211f96df0a9065e1d0
maxilamb <maxilamb@yandex.com>
2020-07-21 02:08:27 +0300

    added vector icons social network

--- src/components/common/footer/index.js
+++ src/components/common/footer/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { LogoFooter } from '../logo';
+import iconInstagram from './images/instagram.svg';
+import iconTelegram from './images/telegram.svg';
 import './style.sass';
 
 function Footer() {
@@ -20,13 +22,13 @@ function Footer() {
           <div className='col-12 col-md-3'>
             <ul className='list-group list-group-horizontal'>
               <li className='list-group-item'>
-                <a href='https://www.instagram.com/explore/tags/juullabs/'>
-                  <i title='Мы в инстаграм' className='fab fa-instagram' />
+                <a href='https://www.instagram.com/explore/tags/juullabs/' title='Мы в инстаграм'>
+                  <img src={iconInstagram} alt='штыефпкфьь social icon' />
                 </a>
               </li>
               <li className='list-group-item'>
-                <a href='https://www.t.me/juullabs'>
-                  <i title='Напишите нам в telegram' className='fab fa-telegram-plane' />
+                <a href='https://www.t.me/juullabs' title='Мы в телеграмме'>
+                  <img src={iconTelegram} alt='telegram social icon' />
                 </a>
               </li>
             </ul>
