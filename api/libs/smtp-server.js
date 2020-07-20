096c6b97bb85b1d98bb24476361e30c5e80002f9
maxilamb <maxilamb@yandex.com>
2020-07-20 19:34:22 +0300

    added auth

--- api/libs/smtp-server.js
+++ api/libs/smtp-server.js
@@ -7,6 +7,26 @@ const server = new SMTPServer({
   disabledCommands: ['STARTTLS', 'AUTH'],
   // Accept messages up to 10 MB
   size: 10 * 1024 * 1024,
+  // Setup authentication
+  // Allow only users with username 'adminsmtp' and password 'smtpadmin'
+  onAuth(auth, session, callback) {
+    const username = 'adminsmtp';
+    const password = 'smtpadmin';
+
+    // check username and password
+    if (
+      auth.username === username &&
+      (auth.method === 'CRAM-MD5'
+        ? auth.validatePassword(password) // if cram-md5, validate challenge response
+        : auth.password === password) // for other methods match plaintext passwords
+    ) {
+      return callback(null, {
+        user: 'userdata',
+      });
+    }
+
+    return callback(new Error('Authentication failed'));
+  },
 });
 
 module.exports = server;
