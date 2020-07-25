06e401d9646d9ef88f17c49eb7c8975ff3f031e7
maxilamb <maxilamb@yandex.com>
2020-07-25 23:05:12 +0300

    refactored index page

--- src/components/routes/index-page/index.js
+++ src/components/routes/index-page/index.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import Juul from '../../sections/juul';
 import Device from '../../sections/device';
-import Taste from '../../sections/taste';
 import Warranty from '../../sections/warranty';
-import Map from '../../sections/map';
 import Feedback from '../../sections/feedback';
+import Charge from '../../sections/charge';
+import Advantages from '../../sections/advantages';
 
 function IndexPage() {
   return (
     <>
-      <Juul />
       <Device />
-      <Taste />
+      <Charge />
+      <Advantages />
       <Warranty />
       <Feedback />
-      <Map />
     </>
   );
 }
