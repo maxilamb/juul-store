7b7b14e0a592cc5eece5991d87ec1b7baaa48021
maxilamb <maxilamb@yandex.com>
2020-07-25 18:59:42 +0300

    added section charge info

---
+++ src/components/sections/charge/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Element } from 'rc-scroll-anim';
+import SectionHeader from '../../common/section-header';
+import chargeSizeDefault from './img/charge.jpg';
+import chargeSizeSmall from './img/charge-small.jpg';
+import './style.sass';
+
+export default function Charge() {
+  return (
+    <Element component='div' className='charge container-fluid px-lg-5 '>
+      <SectionHeader
+        header='Универсальный USB порт для зарядки'
+        text='Литиевая батарея емкостью 280 мАч с LED индикатором'
+      />
+      <ul>
+        <li>Полностью заряжен</li>
+        <li>Средний заряд</li>
+        <li>Слабый заряд</li>
+      </ul>
+      <picture>
+        <source media='(min-width: 768px), 50vw' srcSet={chargeSizeDefault} />
+        <img src={chargeSizeSmall} alt='картника ' />
+      </picture>
+    </Element>
+  );
+}
