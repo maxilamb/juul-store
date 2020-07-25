beb0641573b6344cefe5e3f6a8ca369d32973ce8
maxilamb <maxilamb@yandex.com>
2020-07-25 22:13:59 +0300

    refactored

--- src/components/common/article/index.js
+++ src/components/common/article/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 import './style.sass';
 
 function Article({ header, text, muted = false, center }) {
-  const hClassName = center ? 'text-center' : '';
-  const pClassName = muted ? 'text-muted' : '';
   return (
     <div className='article'>
-      <h3 className={hClassName}>{header}</h3>
-      <p className={pClassName}>{text}</p>
+      <h3
+        className={cx({
+          'text-center': center,
+        })}
+      >
+        {header}
+      </h3>
+      <p
+        className={cx({
+          'text-muted': muted,
+          'text-center': center,
+        })}
+      >
+        {text}
+      </p>
     </div>
   );
 }
