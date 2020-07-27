598529bb9d4f16dbb20d2a93604ae56bdf7598aa
maxilamb <maxilamb@yandex.com>
2020-07-27 13:07:00 +0300

    added logic for button

--- src/components/sections/introduction/index.js
+++ src/components/sections/introduction/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Element } from 'rc-scroll-anim';
 import SectionHeader from '../../common/section-header';
 import allColorsPng from './img/all.png';
 import allColorsW from './img/all.webp';
+import Context from '../../../context';
 import './style.sass';
 
 export default function Introduction() {
+  const { dispatch } = useContext(Context);
   return (
     <Element component='div' id='introduction'>
       <SectionHeader header='JUUL W01' text='Ваша следующая сигарета - не сигарета' />
@@ -13,7 +15,16 @@ export default function Introduction() {
         <h3>
           1999 ₽ <span className='old-price'>2345 ₽</span>
         </h3>
-        <button type='button' className='btn btn-danger btn-lg rounded-pill'>
+        <button
+          type='button'
+          className='btn btn-danger btn-lg rounded-pill'
+          onClick={() => {
+            dispatch({
+              type: 'SET_VISIBLE',
+              payload: true,
+            });
+          }}
+        >
           Купить
         </button>
       </div>
