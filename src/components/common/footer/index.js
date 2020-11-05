c49ff15a7af16f42cf0013f3796f59d69fb9ffaa
Maxim Lambov <maxilamb@yandex.com>
2020-11-05 20:30:46 +0200

    restyled footer

--- src/components/common/footer/index.js
+++ src/components/common/footer/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { LogoFooter } from '../logo';
-import iconInstagram from './images/instagram.svg';
-import iconTelegram from './images/telegram.svg';
 import './style.sass';
 
 function Footer() {
@@ -10,29 +8,17 @@ function Footer() {
     <footer>
       <div className='container'>
         <div className='row'>
-          <div className='col-12 col-md-3 d-flex align-items-center justify-content-center'>
-            <Link to='/terms'>Пользовательское соглашение</Link>
-          </div>
+          <div className='col-12 col-md-3' />
           <div className='col-12 col-md-6 d-flex justify-content-center align-items-center'>
             <div>
               <LogoFooter />
-              <small className='text-muted'>Все права защищены ©</small>
+              <p className='mb-0'>
+                <small className='text-muted'>Все права защищены ©</small>
+              </p>
+              <Link to='/terms'>Пользовательское соглашение</Link>
             </div>
           </div>
-          <div className='col-12 col-md-3'>
-            <ul className='list-group list-group-horizontal'>
-              <li className='list-group-item'>
-                <a href='https://www.instagram.com/explore/tags/juullabs/' title='Мы в инстаграм'>
-                  <img src={iconInstagram} alt='штыефпкфьь social icon' />
-                </a>
-              </li>
-              <li className='list-group-item'>
-                <a href='https://www.t.me/juullabs' title='Мы в телеграмме'>
-                  <img src={iconTelegram} alt='telegram social icon' />
-                </a>
-              </li>
-            </ul>
-          </div>
+          <div className='col-12 col-md-3' />
         </div>
       </div>
     </footer>
